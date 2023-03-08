b6a848d0d65e1f0cb93501a56692437be34b94dc
ozgrfdn@gmail.com <ozgrfdn@gmail.com>
2023-03-08 19:35:32 +0300

    Buttonlar Konuldu

--- EEOE_Satis/fSatis.Designer.cs
+++ EEOE_Satis/fSatis.Designer.cs
@@ -31,11 +31,13 @@
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnNakit = new System.Windows.Forms.Button();
+            this.btnKart = new System.Windows.Forms.Button();
+            this.btnNakitKart = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.SuspendLayout();
@@ -71,8 +73,8 @@
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(493, 750);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
@@ -86,19 +88,19 @@
             this.panel1.Size = new System.Drawing.Size(487, 94);
             this.panel1.TabIndex = 0;
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(9, 54);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 0;
-            // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(115, 54);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 22);
             this.textBox2.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
             // 
             // dataGridView1
             // 
@@ -117,7 +119,9 @@
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
-            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnNakitKart, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnKart, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnNakit, 0, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 558);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
@@ -126,18 +130,56 @@
             this.tableLayoutPanel2.Size = new System.Drawing.Size(487, 189);
             this.tableLayoutPanel2.TabIndex = 2;
             // 
-            // button1
+            // btnNakit
             // 
-            this.button1.BackColor = System.Drawing.Color.Tomato;
-            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Tomato;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(91, 183);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Nakit";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnNakit.BackColor = System.Drawing.Color.Tomato;
+            this.btnNakit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnNakit.FlatAppearance.BorderColor = System.Drawing.Color.Tomato;
+            this.btnNakit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNakit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnNakit.ForeColor = System.Drawing.Color.White;
+            this.btnNakit.Image = global::EEOE_Satis.Properties.Resources.tl_48;
+            this.btnNakit.Location = new System.Drawing.Point(3, 3);
+            this.btnNakit.Name = "btnNakit";
+            this.btnNakit.Size = new System.Drawing.Size(91, 183);
+            this.btnNakit.TabIndex = 0;
+            this.btnNakit.Text = "Nakit\r\n[F1]";
+            this.btnNakit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnNakit.UseVisualStyleBackColor = false;
+            // 
+            // btnKart
+            // 
+            this.btnKart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            this.btnKart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnKart.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            this.btnKart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKart.ForeColor = System.Drawing.Color.White;
+            this.btnKart.Image = global::EEOE_Satis.Properties.Resources.tl_48;
+            this.btnKart.Location = new System.Drawing.Point(100, 3);
+            this.btnKart.Name = "btnKart";
+            this.btnKart.Size = new System.Drawing.Size(91, 183);
+            this.btnKart.TabIndex = 1;
+            this.btnKart.Text = "Kart\r\n[F2]";
+            this.btnKart.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnKart.UseVisualStyleBackColor = false;
+            // 
+            // btnNakitKart
+            // 
+            this.btnNakitKart.BackColor = System.Drawing.Color.Olive;
+            this.btnNakitKart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnNakitKart.FlatAppearance.BorderColor = System.Drawing.Color.Olive;
+            this.btnNakitKart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNakitKart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnNakitKart.ForeColor = System.Drawing.Color.White;
+            this.btnNakitKart.Image = global::EEOE_Satis.Properties.Resources.tl_48;
+            this.btnNakitKart.Location = new System.Drawing.Point(197, 3);
+            this.btnNakitKart.Name = "btnNakitKart";
+            this.btnNakitKart.Size = new System.Drawing.Size(91, 183);
+            this.btnNakitKart.TabIndex = 2;
+            this.btnNakitKart.Text = "Nakit Kart \r\n[F1]";
+            this.btnNakitKart.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnNakitKart.UseVisualStyleBackColor = false;
             // 
             // fSatis
             // 
@@ -168,7 +210,9 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnNakit;
+        private System.Windows.Forms.Button btnNakitKart;
+        private System.Windows.Forms.Button btnKart;
     }
 }
 
